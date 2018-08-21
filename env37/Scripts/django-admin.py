9dce8619899fde0439aa662a5dc9c78ba78e6068
scottrpoulin <scottrpoulin@gmail.com>
2018-08-20 22:13:40 -0500

    again

---
+++ env37/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mochi Flynn\PycharmProjects\ISQA8210\env37\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
